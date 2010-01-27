c7b5b6f44e3a6b8bde6f29a1450cf805d6cfde63
Petr Morávek [Xificurk] <xificurk@gmail.com>
2010-01-27 10:00:37 +0100

    port parrot plugin

--- plugins/parrot.py
+++ plugins/parrot.py
@@ -2,7 +2,7 @@
 """
     plugins/parrot.py - A plugin for making a bot parrot text to MUC or JID.
     Copyright (C) 2007 Kevin Smith
-    Copyright (C) 2009 Petr Morávek
+    Copyright (C) 2009-2010 Petr Morávek
 
     This file is part of KeelsBot.
 
@@ -21,30 +21,28 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-import logging
-import datetime, time
 
 class parrot(object):
     def __init__(self, bot, config):
         self.bot = bot
-        self.config = config
-        self.about = u"'Parrot' umožňuje zaslat vybraným uživatelům text jménem bota do MUCu nebo přímo na JID.\nAutoři: Kevin Smith, Petr Morávek"
+        self.about = "'Parrot' umožňuje zaslat vybraným uživatelům text jménem bota do MUCu nebo přímo na JID.\nAutoři: Kevin Smith, Petr Morávek"
+        bot.addCommand("say", self.say, "Zaslat zprávu do MUCu", "Bot odešle zprávu do zadaného MUCu.", "say muc text")
+        bot.addCommand("tell", self.tell, "Zaslat zprávu JID", "Bot odešle zprávu zadanému JID.", "tell jid text")
 
-        self.bot.addCommand(u'say', self.handle_say, u"Zaslat zprávu do MUCu", u"Bot odešle zprávu do zadaného MUCu.", u'say muc text')
-        self.bot.addCommand(u'tell', self.handle_tell, u"Zaslat zprávu JID", u"Bot odešle zprávu zadanému JID.", u'tell jid text')
 
-    def handle_say(self, command, args, msg):
+    def say(self, command, args, msg):
         if args.count(" ") >= 1:
             [muc, text] = args.split(" ",1)
         else:
-            return u"Nedostatečný počet parametrů."
-        self.bot.sendMessage(muc, text, mtype='groupchat')
-        return u"Odesláno."
+            return "Nedostatečný počet argumentů."
+        self.bot.sendMessage(muc, text, mtype="groupchat")
+        return "Odesláno."
 
-    def handle_tell(self, command, args, msg):
+
+    def tell(self, command, args, msg):
         if args.count(" ") >= 1:
             [jid, text] = args.split(" ",1)
         else:
-            return u"Nedostatečný počet parametrů."
-        self.bot.sendMessage(jid, text, mtype='chat')
-        return u"Odesláno."
+            return "Nedostatečný počet argumentů."
+        self.bot.sendMessage(jid, text, mtype="chat")
+        return "Odesláno."
