dae52087bb81a57c8a3f3ff06f496eb62f5c355c
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-19 00:59:56 +0200

    pastebin plugin update

--- plugins/pastebin.py
+++ plugins/pastebin.py
@@ -1,46 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-    plugins/pastebin.py - A plugin for sending code snippets to pastebin.cz.
-    Copyright (C) 2009-2010 Petr Morávek
+pastebin plugin: Sends code snippetes to pastebin.com.
 
-    This file is part of KeelsBot.
-
-    Keelsbot is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    KeelsBot is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+__author__ = "Petr Morávek (xificurk@gmail.com)"
+__copyright__ = ["Copyright (C) 2009-2011 Petr Morávek"]
+__license__ = "GPL 3.0"
+
+__version__ = "0.5.0"
+
+
 import logging
-import urllib
+import urllib.parse
+import urllib.request
+
+log = logging.getLogger(__name__)
+__ = lambda x: x # Fake gettext function
 
 
-class pastebin(object):
+class pastebin:
     def __init__(self, bot, config):
-        self.log = logging.getLogger("keelsbot.pastebin")
         self.bot = bot
+        self.gettext = self.bot.gettext
+        self.ngettext = self.bot.ngettext
+        config = config.get("config", {})
         self.lang = config.get("lang", "text")
         self.expiration = config.get("expiration", "1D")
-        self.about = "'Pastebin' umožňuje odeslání kódu na pastebin.com.\nAutor: Petr Morávek"
-        self.bot.addCommand("paste", self.paste, "Pastebin", "Odešle kód na pastebin.com.\nNa prvním řádku bere v libovolném pořadí oddělené mezerou platnost (10M = 10 minut, 1H = 1 hodina, 1D = 1 den, 1M = 1 měsíc, N = navždy; výchozí hodnota je {0}), název jazyku (podle http://pastebin.com/api.php, výchozí hodnota je {1}), a zda odeslat vygenerovaný odkaz přímo do MUCu (1, výchozí hodnota je 0, tzn. poslat odkaz zpátky odkud přišel požadavek).\nNa všech dalších řádcích je samotný kód.".format(self.expiration, self.lang), "paste [10M|1H|1D|1M|N] [lang] [0|1]\nKód")
 
+        self.bot.add_command("paste", self.paste, "Pastebin", (__("Sends code snippet to pastebin.com\nAt first line are optional arguments separated by space in an arbitrary order - expiration (10M = 10 minutes, 1H = 1 hour, 1D = 1 day, 1M = 1 month, N = never), langauge format (see http://pastebin.com/api) and whether to send the link directly to MUC (1, defaults to 0 = send the link back to user). Default expiration is "), self.expiration, __(" and language "), self.lang + ".\n", __("All other lines are treated as code content.")), ("[10M|1H|1D|1M|N] [lang] [0|1]\n", __("code")))
 
-    def paste(self, command, args, msg):
+    def paste(self, command, args, msg, uc):
         paste = args.split("\n", 1)
-        self.log.debug(paste)
         if len(paste) < 2:
-            return "Neplatné zadání, mrkni na help."
+            return self.gettext("Invalid input, see help.", uc.lang)
 
-        sendToMUC = 0
+        to_muc = False
         lang = self.lang
         expiration = self.expiration
 
@@ -48,35 +43,36 @@ class pastebin(object):
 
         arguments = paste[0].split(" ")
         for param in arguments:
-            if param.upper() in ["10M", "1H", "1D", "1M", "N"]:
+            if param.upper() in ("10M", "1H", "1D", "1M", "N"):
                 expiration = param.upper()
-            elif param == "0" or param == "1":
-                sendToMUC = int(param)
+            elif param in ("0", "1"):
+                to_muc = param == "1"
             elif param != "":
-                lang = param
+                lang = param.lower()
 
         if msg["type"] == "groupchat":
             author = msg["mucnick"]
-        elif msg["from"].bare in self.bot.rooms:
+        elif "xep_0045" in self.bot.plugin and msg["from"].bare in self.bot.plugin["xep_0045"].getJoinedRooms():
             author = msg["from"].resource
+        elif "nick" in msg.keys():
+            author = msg["nick"]
         else:
             author = msg["from"].user
 
         data = {"paste_code":code, "paste_name":author, "paste_expire_date":expiration, "paste_format":lang}
-        self.log.debug(data)
+        log.debug(data)
 
         try:
-            response = urllib.request.urlopen("http://pastebin.com/api_public.php", urllib.parse.urlencode(data), 10)
+            response = urllib.request.urlopen("http://pastebin.com/api_public.php", urllib.parse.urlencode(data).encode("utf-8"), 10).read().decode("utf-8")
         except IOError:
-            self.log.error("Could not fetch pastebin.")
-            return "ERROR"
-        if response.getcode() != 200:
-            self.log.error("Got error code {0} from pastebin.".format(response.getcode()))
-            return "ERROR {0}".format(response.getcode())
+            log.error("Could not fetch pastebin.")
+            return self.gettext("ERROR", uc.lang)
+        log.debug(response)
+        url = response.split("\n", 1)[0]
+        if not url.startswith("http://pastebin.com/"):
+            return self.gettext("ERROR", uc.lang)
 
-        url = response.readline().decode("utf-8")
-
-        if sendToMUC == 1 and msg["type"] != "groupchat" and msg["from"].bare in self.bot.rooms:
-            self.bot.sendMessage(msg["from"].bare, "{0} vložil {1}".format(author, url), mtype="groupchat")
+        if to_muc and msg["type"] != "groupchat" and "xep_0045" in self.bot.plugin and msg["from"].bare in self.bot.plugin["xep_0045"].getJoinedRooms():
+            self.bot.send_message(msg["from"].bare, self.gettext("{} sent code snippet {}", uc.lang).format(author, url), mtype="groupchat")
         else:
             return str(url)
