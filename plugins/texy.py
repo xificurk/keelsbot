32bd4b8d20b1b1957e6c3f4fccf8f202dd1399e2
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-19 00:23:25 +0200

    texy plugin update

--- plugins/texy.py
+++ plugins/texy.py
@@ -1,36 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-    plugins/texy.py - A plugin for parsing Texy! syntax.
-    Copyright (C) 2010 Petr Morávek
+texy plugin: Parse Texy! syntax.
 
-    This file is part of KeelsBot.
-
-    Keelsbot is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    KeelsBot is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+__author__ = "Petr Morávek (xificurk@gmail.com)"
+__copyright__ = ["Copyright (C) 2009-2011 Petr Morávek"]
+__license__ = "GPL 3.0"
+
+__version__ = "0.5.0"
+
+
+import logging
 import os.path
 from subprocess import getoutput
 
+log = logging.getLogger(__name__)
+__ = lambda x: x # Fake gettext function
 
-class texy(object):
+
+class texy:
     def __init__(self, bot, config):
-        self.texy = "php {0}".format(os.path.join(os.path.dirname(__file__), "..", "misc", "texy.process.php"))
-        self.about = "'Texy' parsuje zprávy pomocí Texy!.\nAutor: Petr Morávek"
-        bot.addCommand("texy", self.process, "Texy!", "Zpracuje zprávu pomocí Texy! Pokud není předána žádná zpráva, vypíše verzi Texy!", "texy [zpráva]")
+        self.texy = "php {}".format(os.path.join(os.path.dirname(__file__), "..", "misc", "texy.process.php"))
+        bot.add_command("texy", self.process, "Texy!", __("Return the message processed by Texy! If not message is given, return Texy! version."), __("[message]"))
 
-    def process(self, command, args, msg):
+    def process(self, command, args, msg, uc):
         if args == "":
-            return "Texy! {0}".format(getoutput(self.texy))
-        return getoutput("{0} '{1}'".format(self.texy, args.replace("'", "\\'")))
+            return "Texy! {}".format(getoutput(self.texy))
+        return getoutput("{} '{}'".format(self.texy, args.replace("'", "\\'")))
