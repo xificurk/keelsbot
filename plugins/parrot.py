6bb55e2182d5cf7878d06fef77e3cb83eb7bb600
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-22 00:19:19 +0200

    parrot plugin update

--- plugins/parrot.py
+++ plugins/parrot.py
@@ -20,21 +20,22 @@ __ = lambda x: x # Fake gettext function
 
 class parrot:
     def __init__(self, bot, config):
-        self.bot = bot
-        self.gettext = self.bot.gettext
-        self.ngettext = self.bot.ngettext
+        self.send_message = bot.send_message
+        self.get_our_nick = bot.get_our_nick
+        self.gettext = bot.gettext
+        self.ngettext = bot.ngettext
 
         bot.add_command("say", self.say, __("Send message into MUC"), __("Bot sends the groupchat message into the specified MUC."), __("room@server text"))
         bot.add_command("tell", self.tell, __("Send message to JID"), __("Bot sends the message to the specified JID."), __("user@server text"))
 
     def say(self, command, args, msg, uc):
         if args.count(" ") >= 1:
-            muc, text = args.split(" ", 1)
+            room, text = args.split(" ", 1)
         else:
             return self.gettext("You've forgot to specify the MUC.", uc.lang)
-        if "xep_0045" not in self.bot.plugin or muc not in self.bot.plugin["xep_0045"].getJoinedRooms():
-            return self.gettext("I'm not in the room {}.", uc.lang).format(muc)
-        self.bot.send_message(muc, text, mtype="groupchat")
+        if self.get_our_nick(room):
+            return self.gettext("I'm not in the room {}.", uc.lang).format(room)
+        self.send_message(room, text, mtype="groupchat")
         return self.gettext("Sent", uc.lang)
 
     def tell(self, command, args, msg, uc):
@@ -42,5 +43,5 @@ class parrot:
             jid, text = args.split(" ", 1)
         else:
             return self.gettext("You've forgot to specify the JID.", uc.lang)
-        self.bot.send_message(jid, text, mtype="chat")
+        self.send_message(jid, text, mtype="chat")
         return self.gettext("Sent", uc.lang)
