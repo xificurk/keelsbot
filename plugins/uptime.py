c346bc2a9befb6aab8984b456272de000e671019
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-22 00:28:46 +0200

    uptime plugin update

--- plugins/uptime.py
+++ plugins/uptime.py
@@ -28,8 +28,6 @@ class uptime:
 
     def uptime(self, command, args, msg, uc):
         delta = self.format_timedelta(time.time() - self.started, uc.lang)
-        log.debug(delta)
-        log.debug("I'm up {}.".format(delta))
         return self.gettext("I'm up {}.", uc.lang).format(delta)
 
     def format_timedelta(self, delta, lang):
@@ -38,29 +36,29 @@ class uptime:
         months = int(delta/3600/24/30)
         if months > 0:
             delta -= months*3600*24*30
-            parts.append(self.ngettext("{} month", "{} months", months, lang).format(months))
+            parts.append(self.ngettext("already {} month", "already {} months", months, lang).split(" ", 1)[1].format(months))
 
         days = int(delta/3600/24)
         if days > 0 or len(parts) > 0:
             delta -= days*3600*24
-            parts.append(self.ngettext("{} day", "{} days", days, lang).format(days))
+            parts.append(self.ngettext("already {} day", "already {} days", days, lang).split(" ", 1)[1].format(days))
         if len(parts) > 1:
             return self.gettext(" and ", lang).join(parts)
 
         hours = int(delta/3600)
         if hours > 0 or len(parts) > 0:
             delta -= hours*3600
-            parts.append(self.ngettext("{} hour", "{} hours", hours, lang).format(hours))
+            parts.append(self.ngettext("already {} hour", "already {} hours", hours, lang).split(" ", 1)[1].format(hours))
         if len(parts) > 1:
             return self.gettext(" and ", lang).join(parts)
 
         minutes = int(delta/60)
         if minutes > 0 or len(parts) > 0:
             delta -= minutes*60
-            parts.append(self.ngettext("{} minute", "{} minutes", minutes, lang).format(minutes))
+            parts.append(self.ngettext("already {} minute", "already {} minutes", minutes, lang).split(" ", 1)[1].format(minutes))
         if len(parts) > 1:
             return self.gettext(" and ", lang).join(parts)
 
         seconds = int(delta)
-        parts.append(self.ngettext("{} second", "{} seconds", seconds, lang).format(seconds))
+        parts.append(self.ngettext("already {} second", "already {} seconds", seconds, lang).split(" ", 1)[1].format(seconds))
         return self.gettext(" and ", lang).join(parts)
