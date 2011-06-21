a62bc5adf51d12c4a616445183dd136fec54069b
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-22 00:12:40 +0200

    help plugin update

--- plugins/help.py
+++ plugins/help.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-help plugin: Displays help and other topics.
+help plugin: Displays help for commands and other topics.
 
 """
 
@@ -19,21 +19,24 @@ __ = lambda x: x # Fake gettext function
 
 class help:
     def __init__(self, bot, config):
-        self.bot = bot
-        self.gettext = self.bot.gettext
-        self.ngettext = self.bot.ngettext
+        self.cmd_prefix = bot.cmd_prefix
+        self.commands = bot.commands
+        self.get_command_level = bot.get_command_level
+        self.help_topics = bot.help_topics
+        self.gettext = bot.gettext
+        self.ngettext = bot.ngettext
 
         bot.add_command("help", self.help, __("Help"), __("If no topic was given, display the list of available commands and other help topics. Otherwise display the help for a given topic."), __("[command/topic]"))
         bot.add_command("commands", self.commands, __("Commands"), __("Display list of available commands."))
 
     def commands(self, command, args, msg, uc):
         response = self.gettext("Available commands", uc.lang) + ":\n"
-        for cmd in sorted(self.bot.commands.keys()):
-            if self.bot.permissions["command:"+cmd] > uc.level:
+        for cmd in sorted(self.commands.keys()):
+            if self.get_command_level(cmd) > uc.level:
                 continue
-            response += self.bot.cmd_prefix + cmd
-            if self.bot.help_topics[cmd].title is not None:
-                response += " -- " + self.gettext(self.bot.help_topics[cmd].title, uc.lang)
+            response += self.cmd_prefix + cmd
+            if self.help_topics[cmd].title is not None:
+                response += " -- " + self.gettext(self.help_topics[cmd].title, uc.lang)
             response += "\n"
         response = response.strip("\n")
         return response
@@ -43,26 +46,26 @@ class help:
         if len(args) == 0:
             response += self.commands(command, args, msg, uc) + "\n"
             start = True
-            for topic in sorted(self.bot.help_topics.keys()):
-                if topic in self.bot.commands:
+            for topic in sorted(self.help_topics.keys()):
+                if topic in self.commands:
                     continue
                 if start:
                     response += "\n" + self.gettext("Other available help topics", uc.lang) + ":\n"
                     start = False
                 response += topic
-                if self.bot.help_topics[topic].title is not None:
-                    response += " -- " + self.gettext(self.bot.help_topics[topic].title, uc.lang)
+                if self.help_topics[topic].title is not None:
+                    response += " -- " + self.gettext(self.help_topics[topic].title, uc.lang)
                 response += "\n"
             args = "help"
             response += "---------\n"
 
-        if args.startswith(self.bot.cmd_prefix) and len(args) > len(self.bot.cmd_prefix):
-            args = args[len(self.bot.cmd_prefix):]
+        if args.startswith(self.cmd_prefix) and len(args) > len(self.cmd_prefix):
+            args = args[len(self.cmd_prefix):]
 
-        if args not in self.bot.help_topics or (args in self.bot.commands and uc.level < self.bot.permissions["command:"+args]):
+        if args not in self.help_topics or (args in self.commands and uc.level < self.get_command_level(args)):
             return self.gettext("Don't know...", uc.lang)
 
-        help_topic = self.bot.help_topics[args]
+        help_topic = self.help_topics[args]
         if help_topic.title is not None:
             response += self.gettext(help_topic.title, uc.lang) + "\n"
         if help_topic.body is not None:
