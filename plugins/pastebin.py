24e8da257d54840cefa6c1841bc0245404451c84
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-22 00:05:58 +0200

    pastebin plugin update

--- plugins/pastebin.py
+++ plugins/pastebin.py
@@ -20,15 +20,19 @@ __ = lambda x: x # Fake gettext function
 
 
 class pastebin:
-    def __init__(self, bot, config):
-        self.bot = bot
-        self.gettext = self.bot.gettext
-        self.ngettext = self.bot.ngettext
-        config = config.get("config", {})
-        self.lang = config.get("lang", "text")
-        self.expiration = config.get("expiration", "1D")
+    lang = "text"
+    expiration = "1D"
 
-        self.bot.add_command("paste", self.paste, "Pastebin", (__("Sends code snippet to pastebin.com\nAt first line are optional arguments separated by space in an arbitrary order - expiration (10M = 10 minutes, 1H = 1 hour, 1D = 1 day, 1M = 1 month, N = never), langauge format (see http://pastebin.com/api) and whether to send the link directly to MUC (1, defaults to 0 = send the link back to user). Default expiration is "), self.expiration, __(" and language "), self.lang + ".\n", __("All other lines are treated as code content.")), ("[10M|1H|1D|1M|N] [lang] [0|1]\n", __("code")))
+    def __init__(self, bot, config):
+        self.get_our_nick = bot.get_our_nick
+        self.gettext = bot.gettext
+        self.ngettext = bot.ngettext
+
+        config = config.get("config", {})
+        self.lang = config.get("lang", self.lang)
+        self.expiration = config.get("expiration", self.expiration)
+
+        bot.add_command("paste", self.paste, "Pastebin", (__("Sends code snippet to pastebin.com\nAt first line are optional arguments separated by space in an arbitrary order - expiration (10M = 10 minutes, 1H = 1 hour, 1D = 1 day, 1M = 1 month, N = never), langauge format (see http://pastebin.com/api) and whether to send the link directly to MUC (1, defaults to 0 = send the link back to user). Default expiration is "), self.expiration, __(" and language "), self.lang + ".\n", __("All other lines are treated as code content.")), ("[10M|1H|1D|1M|N] [lang] [0|1]\n", __("code")))
 
     def paste(self, command, args, msg, uc):
         paste = args.split("\n", 1)
@@ -52,7 +56,7 @@ class pastebin:
 
         if msg["type"] == "groupchat":
             author = msg["mucnick"]
-        elif "xep_0045" in self.bot.plugin and msg["from"].bare in self.bot.plugin["xep_0045"].getJoinedRooms():
+        elif self.get_our_nick(msg["from"].bare) is not None:
             author = msg["from"].resource
         elif "nick" in msg.keys():
             author = msg["nick"]
@@ -63,16 +67,17 @@ class pastebin:
         log.debug(data)
 
         try:
-            response = urllib.request.urlopen("http://pastebin.com/api_public.php", urllib.parse.urlencode(data).encode("utf-8"), 10).read().decode("utf-8")
-        except IOError:
-            log.error("Could not fetch pastebin.")
-            return self.gettext("ERROR", uc.lang)
-        log.debug(response)
-        url = response.split("\n", 1)[0]
-        if not url.startswith("http://pastebin.com/"):
+            data = urllib.parse.urlencode(data).encode("utf-8")
+            response = urllib.request.urlopen("http://pastebin.com/api_public.php", data, 10).read().decode("utf-8")
+            url = str(response.split("\n", 1)[0])
+            if not url.startswith("http://pastebin.com/"):
+                raise ValueError(_("Unexpected response:") + "\n{}".format(response))
+        except:
+            log.exception(_("Unexpected error while getting pastebin URL."))
             return self.gettext("ERROR", uc.lang)
 
-        if to_muc and msg["type"] != "groupchat" and "xep_0045" in self.bot.plugin and msg["from"].bare in self.bot.plugin["xep_0045"].getJoinedRooms():
-            self.bot.send_message(msg["from"].bare, self.gettext("{} sent code snippet {}", uc.lang).format(author, url), mtype="groupchat")
+        if to_muc and msg["type"] != "groupchat" and self.get_our_nick(msg["from"].bare) is not None:
+            msg["type"] = "groupchat"
+            return self.gettext("{} sent code snippet {}", uc.lang).format(author, url)
         else:
-            return str(url)
+            return url
